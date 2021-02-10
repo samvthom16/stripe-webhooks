32b8b973b1d2200962d43b6d66a7bca1fb57dbf1
unknown <>
2021-02-10 21:24:11 +0530

    Webhooks display in settings

--- templates/settings-general.php
+++ templates/settings-general.php
@@ -24,5 +24,16 @@
 		<input type="text" name="<?php _e( $option_name );?>" style="width: 100%; max-width: 400px;" value="<?php _e( $option_value );?>" />
 	</p>
 	<?php endforeach;?>
+	<p>Setup webhook for this URL: <span class="badge"><?php _e( admin_url( 'admin-ajax.php' ) . '?action=stripe-webhooks' );?></span></p>
 	<p class='submit'><input type="submit" name="submit" class="button button-primary" value="Save Settings"><p>
 </form>
+<style>
+	form p span.badge{
+		border: #ccc;
+		background: #ccc;
+		margin-left: 10px;
+		padding: 3px 7px;
+		display: inline;
+		border-radius: 5px;
+	}
+</style>
