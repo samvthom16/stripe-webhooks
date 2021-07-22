5e5e7d66365fcd68704b9f0da4229e819bca20e0
unknown <>
2021-07-22 11:07:59 +0530

    Refactor settings admin

--- templates/stripe-mailchimp-webhooks.php
+++ templates/stripe-mailchimp-webhooks.php
@@ -1,71 +1,12 @@
 <?php
 
 	$screens = array(
-		'general'	=> array(
-			'label'		=> 'General Settings',
-			'tab'		=> plugin_dir_path(__FILE__).'settings-general.php'
-		),
-		'stores'	=> array(
-			'label'		=> 'Mailchimp Stores',
-			'action'	=> 'stores',
-			'tab'		=> plugin_dir_path(__FILE__).'settings-stores.php'
-		),
-		'payments'	=> array(
-			'label'		=> 'Recent Payments',
-			'action'	=> 'payments',
-			'tab'			=> plugin_dir_path(__FILE__).'settings-payments.php'
-		),
-		'invoices'	=> array(
-			'label'		=> 'Recent Invoices',
-			'action'	=> 'invoices',
-			'tab'			=> plugin_dir_path(__FILE__).'settings-invoices.php'
-		),
-		'order_info'	=> array(
-			'label'		=> 'Get Order Info',
-			'action'	=> 'order_info',
-			'tab'			=> plugin_dir_path(__FILE__).'settings-order-info.php'
-		),
+		'general'		=> 'General Settings',
+		'stores'		=> 'Mailchimp Stores',
+		'payments'	=> 'Recent Payments',
+		'invoices'	=> 'Recent Invoices',
 	);
 
-	$screens = apply_filters( 'meteor_admin_settings_screens', $screens );
+	$this->displayTabs( $screens, 'Stripe Mailchimp Webhooks', plugin_dir_path( __FILE__ ) . "settings" );
 
-	$active_tab = '';
-?>
-<div class="wrap">
-	<h1>Stripe Webhooks Settings</h1>
-	<h2 class="nav-tab-wrapper">
-	<?php
-		foreach( $screens as $slug => $screen ){
-
-			$base_settings_url = "admin.php?page=stripe-mailchimp-webhooks";
-
-			$url = admin_url( $base_settings_url );
-
-			if( isset( $screen['action'] ) ){
-				$url =  esc_url( add_query_arg( array( 'action' => $screen['action'] ), admin_url( $base_settings_url ) ) );
-			}
-
-			$nav_class = "nav-tab";
-
-			if( isset( $screen['action'] ) && isset( $_GET['action'] ) && $screen['action'] == $_GET['action'] ){
-				$nav_class .= " nav-tab-active";
-				$active_tab = $slug;
-			}
-
-			if( ! isset( $screen['action'] ) && ! isset( $_GET['action'] ) ){
-				$nav_class .= " nav-tab-active";
-				$active_tab = $slug;
-			}
-
-			echo '<a href="'.$url.'" class="'.$nav_class.'">'.$screen['label'].'</a>';
-		}
-	?>
-	</h2>
-	<?php
-
-		if( file_exists( $screens[ $active_tab ][ 'tab' ] ) ){
-			include( $screens[ $active_tab ][ 'tab' ] );
-		}
-
-	?>
-</div>
+	
