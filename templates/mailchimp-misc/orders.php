9c2823a8a35e3a7c3fd09d3b83d87d9e80114574
unknown <>
2021-07-29 16:50:29 +0530

    Change UI to table for orders

--- templates/mailchimp-misc/orders.php
+++ templates/mailchimp-misc/orders.php
@@ -4,8 +4,39 @@
 
 	$store_id = $mailchimpAPI->getStoreID();
 
-	$response = $mailchimpAPI->cachedProcessRequest( "/ecommerce/stores/$store_id/orders" );
+	$columns = array(
+		array(
+			'label'	=> 'ID',
+			'key'		=> 'id'
+		),
+		array(
+			'label'	=> 'First Name',
+			'key'		=> 'customer->first_name'
+		),
+		array(
+			'label'	=> 'Last Name',
+			'key'		=> 'customer->last_name'
+		),
+		array(
+			'label'	=> 'Order Total',
+			'key'		=> 'order_total'
+		),
+		array(
+			'label'	=> 'Processed At Foreign',
+			'key'		=> 'processed_at_foreign'
+		),
+	);
 
-	echo "<pre>";
-	print_r( $response );
-	echo "</pre>";
+	$per_page = 50;
+	$activepage = isset( $_GET['paged'] ) ? $_GET['paged'] : 1;
+	$offset = ( $activepage - 1 ) * $per_page;
+	$response = $mailchimpAPI->cachedProcessRequest( "/ecommerce/stores/$store_id/orders?count=$per_page&offset=$offset" );
+
+	$table_ui = STRIPE_WEBHOOKS_TABLE_UI::getInstance();
+	$table_ui->display( $columns, $response->orders, 'orders' );
+
+	$table_ui->pagination( $per_page, $response->total_items, array( 'action' ) );
+
+	//echo "<pre>";
+	//print_r( $response );
+	//echo "</pre>";
